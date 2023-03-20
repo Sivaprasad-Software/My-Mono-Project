6172ae7486cc7b3ef3fe93c736803818f2aa7b50
Gitbysiva <118656180+Sivaprasad-Software@users.noreply.github.com>
2023-03-20 19:53:16 +0530

    Update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-west-2"
-  access_key = "AKIATAGFDTG46ABGG46K"
-  secret_key = "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"
+  access_key = "AKIATAGFDTG4ZTYU2C73"
+  secret_key = "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"
 }
